d9235a2f4f329033d6842e19a730c5fdd818c573
Brayan Angarita <brayanangarita11@gmail.com>
2019-12-10 16:57:58 -0500

    UTF-8 to email

--- email.php
+++ email.php
@@ -1,139 +1,38 @@
-
 <?php
-		if($_POST["name"]==""||$_POST["last_name"]==""||$_POST["email"]==""||$_POST["sex"]==""||$_POST["date"]==""){
+	if($_POST["name"]==""||$_POST["last_name"]==""||$_POST["email"]==""||$_POST["sex"]==""||$_POST["date"]==""){
 
-			echo "Lo sentimos, no llenaste toda la información";
-		} else {
-			//echo "hágale mijo";
-			//Check if the "Sender's Email" input field is filled out
-			$email=$_POST['email'];
-			// Sanitize E-mail Address
-			$email =filter_var($email, FILTER_SANITIZE_EMAIL);
-			// Validate E-mail Address
-			$email= filter_var($email, FILTER_VALIDATE_EMAIL);
+		echo "Lo sentimos, no llenaste toda la información";
+	} else {
+		$email=$_POST['email'];
+		// Sanitize E-mail Address
+		$email =filter_var($email, FILTER_SANITIZE_EMAIL);
 
-			$name = $_POST['name'];
-			$last_name = $_POST['last_name'];
-			//$email = $_POST['email'];
-			//$email =filter_var($email, FILTER_SANITIZE_EMAIL);
-			$sex = $_POST['sex'];
-			$date = $_POST['date'];
+		$name = $_POST['name'];
+		$last_name = $_POST['last_name'];
+		$sex = $_POST['sex'];
+		$date = $_POST['date'];
 
-			if (!$email){
-				echo "Invalid Sender's Email";
-			}
-			else{
-				$subject = 'Nuevo registro Wowzer';
-				$message = 'Hola Wowzer! '. $name . ' ' . $last_name . ' con el correo electrónico ' . $email . ', nacido en ' . $date . ' y de sexo <b>' .  $sex . '</b> desea inscribirse a tu lista de MailChimp';
-				$headers  = 'MIME-Version: 1.0' . "\r\n";
-				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-				$headers = 'From:'. 'wowzers.store@gmail.com' ."\r\n".
-							'Reply-To: '.$from."\r\n" .
-							'X-Mailer: PHP/' . phpversion(); // Sender's Email
-				//$headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
-				// Message lines should not exceed 70 characters (PHP rule), so wrap it
-				$message = wordwrap($message, 150);
-				// Send Mail By PHP Mail Function
-				mail("brayanangarita11@gmail.com", $subject, $message, $headers);
-				echo "Gracias, ahora eres parte de nuestra lista";
-			}
+		if (!$email){
+			echo "Invalid Sender's Email";
 		}
-	
-?>
+		else{
+			$subject = 'Nuevo registro Wowzer';
+			$headers  = 'MIME-Version: 1.0' . "\r\n";
+			$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
+			$headers = 'From:'. 'wowzers.store@gmail.com' ."\r\n".
+						'Reply-To: '.$email."\r\n" .
+						'X-Mailer: PHP/' . phpversion(); // Sender's Email
+			//$headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
 
+			$message = '<html><body>';
+			$message .= 'Hola Wowzer! '. $name . ' ' . $last_name . ' con el correo electrónico ' . $email . ', nacido en ' . $date . ' y de sexo <b>' .  $sex . '</b> desea inscribirse a tu lista de MailChimp';
 
+			$message .= '</body></html>';
 
-
-<?php
-//use PHPMailer\PHPMailer\PHPMailer;
-//use PHPMailer\PHPMailer\SMTP;
-//use PHPMailer\PHPMailer\Exception;
-
-//require 'vendor/phpmailer/phpmailer/src/Exception.php';
-//require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-//require 'vendor/phpmailer/phpmailer/src/SMTP.php';
-
-//sendMail();
-
-//function sendMail(){
-
-	//if($_POST["name"]==""||$_POST["last_name"]==""||$_POST["email"]==""||$_POST["sex"]==""||$_POST["date"]==""){
-
-		//echo "Lo sentimos, no llenaste toda la información";
-	//} else {
-
-
-		//$name = $_POST['name'];
-		//$last_name = $_POST['last_name'];
-		//$email = $_POST['email'];
-		//$email =filter_var($email, FILTER_SANITIZE_EMAIL);
-		//$sex = $_POST['sex'];
-		//$date = $_POST['date'];
-
-		//$mail = new PHPMailer(true);
-
-		//try {
-		    //Server settings
-		    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-		    //$mail->isSMTP();                                            // Send using SMTP
-		    //$mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-		    //$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-		    //$mail->Username   = 'wowzers.store@gmail.com';                     // SMTP username
-		    //$mail->Password   = '';                               // SMTP password
-		   // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-		    //$mail->Port       = 587;                                    // TCP port to connect to
-
-		    //Recipients
-		    //$mail->setFrom('wowzers.store@gmail.com', 'Wowzers');
-		    //$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-		    //$mail->addAddress('brayanangarita11@gmail.com', 'Wowzers'); //info@wowzers.co               // Name is optional
-		    //$mail->addReplyTo('info@example.com', 'Information');
-		    //$mail->addCC('cc@example.com');
-		    //$mail->addBCC('bcc@example.com');
-
-		    // Attachments
-		    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-		    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-		    // Content
-		    //$mail->isHTML(true);                                  // Set email format to HTML
-		    //$mail->Subject = 'Registro de Wowzer';
-		    // $mail->Body    = 'Hola Wowzer! '. $name . ' ' . $last_name .
-		    // 				 ' con el correo electrónico ' . $email . ', nacido en ' . $date . ' y de sexo <b>' .  $sex . '</b> desea inscribirse a tu lista de MailChimp';
-
-		    // $mail->AltBody = 'Hola Wowzer! '. $name . $last_name .
-		    // 				 ' con el correo electrónico' . $email . ' nacido en ' . $date . ' y de sexo ' . $sex . ' desea inscribirse a tu lista de MailChimp';
-
-		    // $mail->send();
-		    //echo 'Gracias, ahora eres parte de nuestra lista';
-
-		    ///header('Location: https://www.facebook.com/');
-
-
-	// 	} catch (Exception $e) {
-	// 	    echo "Lo sentimos, no hemos podido agregarte a la lista. Intenta nuevamente. Si sigues teniendo el mismo error por favor envíanos un mensaje al correo info@wowzers.co diciendo que el error que tienes es: {$mail->ErrorInfo}";
-	// 	}
-
-	// }
-
-
-
-	// } else {
-	// 	return;
-	// }
-
-//}
-	// 	if (isset($_POST['name']) && 
-	//echo $name;
-	// 	(isset($_POST['last_name']) && 
-	// 	(isset($_POST['email']) && 
-	// 	(isset($_POST['sex']) && 
-	// 	(isset($_POST['date'])){
-	// 	echo 'No error';
-	// 	} else {
-	// 		echo 'Error';
-	// 	}
-	// }
+			// Send Mail By PHP Mail Function
+			mail("brayanangarita11@gmail.com", $subject, $message, $headers);
+			echo "Gracias, ahora eres parte de nuestra lista";
+		}
+	}
 ?>
